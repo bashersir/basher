6ce02b557355c5a15c25ab51377842f99c820a1a
Abul Basher <basher.send@gmail.com>
2022-09-24 16:24:20 +0600

    text addad

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <h1>My React App</h1>
       <h2>Author: Muhammad Abul Basher, Lecturer in English</h2>
+      <p>Transforming Lives We are the only project in Bangladesh offering a safe haven and livelihood training to single mothers and their children, transforming their lives with independence, confidence and dignity. Sreepur Village has becoming increasingly well known throughout the country for empowering mothers and their children with hope for a brighter future</p>
       <img src={profileImage} alt="profile-pic" />
     </div>
   );
