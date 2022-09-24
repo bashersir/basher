e56b8ee52c37e957002098225ecf8f7ef5048a37
Abul Basher <basher.send@gmail.com>
2022-09-24 17:38:38 +0600

    desc02 updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import React from 'react';
 import './App.css';
-import profileImage1 from './img/abul_basher.jpg';
 import profileImage2 from './img/basher.jpg';
 import profileImage01 from './img/basher01.jpg';
 import profileImage02 from './img/basher02.jpg';
@@ -11,8 +10,7 @@ function App() {
     <div className="App">
       <h1>Travelling to Green Valley Park, Ruppur, Nator 2022</h1>
       <h2>Organized by a Team of Teachers</h2>
-      <p>Green Valley Park is situated in Lalpur, Natore, Bangladesh. It is an amazing place to visit. I am really surprised visiting this place first time. It was a really nice trip. Want to go there again with my family. Hope, you will enjoy here.</p>
-      <img className="img1" src={profileImage1} alt="profile-pic1" />
+      <p className='des01'>Green Valley Park is situated in Lalpur, Natore, Bangladesh. It is an amazing place to visit. I am really surprised visiting this place first time. It was a really nice trip. Want to go there again with my family. Hope, you will enjoy here.</p>
       <img className="img2" src={profileImage2} alt="profile-pic2" />
       <img className="img3" src={profileImage01} alt="profile-pic3" />
       <img className="img4" src={profileImage02} alt="profile-pic4" />
